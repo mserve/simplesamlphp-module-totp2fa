f6f7d80598c70f9c5b8b3574ff1c7047d61608e7
Martin Stuckenbroeker <stuckenbroeker@oszimt.de>
2021-03-20 18:42:12 +0100

    add install_dependencies script

---
+++ bin/install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Install composer
+if [ ! -x "bin/composer.phar" ]; then
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=bin/
+fi
+
+# Install dependencies (without vcs history or dev tools)
+php "bin/composer.phar" install --no-dev --prefer-dist -o -d
